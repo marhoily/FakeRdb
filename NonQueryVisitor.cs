047fa76a983c913f948924a38327f641bbe62257
Ilya <marhoily@gmail.com>
2023-09-16 20:10:44 +0300

    columns parsed

--- NonQueryVisitor.cs
+++ NonQueryVisitor.cs
@@ -1,3 +1,5 @@
+using static SQLiteParser;
+
 namespace FakeRdb;
 
 public sealed class NonQueryVisitor : SQLiteParserBaseVisitor<int>
@@ -12,7 +14,24 @@ public sealed class NonQueryVisitor : SQLiteParserBaseVisitor<int>
     public override int VisitCreate_table_stmt(SQLiteParser.Create_table_stmtContext context)
     {
         var tableName = context.table_name().GetText();
-        _db.Add(tableName, new Table(Array.Empty<Field>()));
+        var fields = context.column_def().Select(col =>
+                new Field(col.column_name().GetText(),
+                    col.type_name().ToRuntimeType()))
+            .ToArray();
+        _db.Add(tableName, new Table(fields));
         return base.VisitCreate_table_stmt(context);
+    }
+}
+
+public static class TypeExt
+{
+    public static Type ToRuntimeType(this Type_nameContext context)
+    {
+        return context.GetText() switch
+        {
+            "TEXT" => typeof(string),
+            "INTEGER" => typeof(int),
+            var x => throw new ArgumentOutOfRangeException(x)
+        };
     }
 }
