e0fafcdf4020f9207f8b5ccdbbdc9c289d207436
Ilya <marhoily@hotmail.com>
2023-09-16 10:23:55 +0300

    + sqlite version

---
+++ AdoNetProvider.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace FakeRdb;
+
+public sealed class ToySqliteConnection : DbConnection
+{
+    private string _connectionString;
+    private ConnectionState _state;
+
+    public ToySqliteConnection(string connectionString)
+    {
+        _connectionString = connectionString;
+        _state = ConnectionState.Closed;
+    }
+
+    public override string ConnectionString
+    {
+        get { return _connectionString; }
+        set
+        {
+            if (_state != ConnectionState.Closed)
+                throw new InvalidOperationException("The connection state must be closed to set the connection string.");
+            _connectionString = value;
+        }
+    }
+
+    public override string Database { get; }
+    public override string DataSource { get; }
+    public override string ServerVersion { get; }
+    public override ConnectionState State => _state;
+
+    protected override DbCommand CreateDbCommand()
+    {
+        return new ToySqliteCommand(this);
+    }
+
+    public override void Open()
+    {
+        if (_state == ConnectionState.Open)
+            throw new InvalidOperationException("The connection is already open.");
+        _state = ConnectionState.Open;
+    }
+
+    public override void Close()
+    {
+        _state = ConnectionState.Closed;
+    }
+
+    protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ChangeDatabase(string databaseName)
+    {
+        throw new NotSupportedException("Changing the database is not supported in this toy SQLite provider.");
+    }
+}
+
+public class ToySqliteCommand : DbCommand
+{
+    private ToySqliteConnection connection;
+    private string commandText;
+
+    public ToySqliteCommand(ToySqliteConnection connection)
+    {
+        this.connection = connection;
+    }
+
+    public override void Prepare()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string CommandText
+    {
+        get { return commandText; }
+        set { commandText = value; }
+    }
+
+    public override int CommandTimeout { get; set; }
+    public override CommandType CommandType { get; set; }
+    public override UpdateRowSource UpdatedRowSource { get; set; }
+    protected override DbConnection? DbConnection { get; set; }
+    protected override DbParameterCollection DbParameterCollection { get; }
+    protected override DbTransaction? DbTransaction { get; set; }
+    public override bool DesignTimeVisible { get; set; }
+
+    protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int ExecuteNonQuery()
+    {
+        throw new NotSupportedException("ExecuteNonQuery is not supported in this toy SQLite provider.");
+    }
+
+    public override object ExecuteScalar()
+    {
+        if (connection.State != ConnectionState.Open)
+            throw new InvalidOperationException("The connection must be open to execute a command.");
+        
+        // Execute the scalar query and return the result
+        Console.WriteLine("Executing query: " + commandText);
+        return "ToySQLiteResult";
+    }
+
+    public override void Cancel()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override DbParameter CreateDbParameter()
+    {
+        throw new NotSupportedException("Creating parameters is not supported in this toy SQLite provider.");
+    }
+
+    // Other methods and properties omitted for brevity...
+}
--- SimpleSelectTests.cs
+++ SimpleSelectTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Microsoft.Data.Sqlite;
 
 namespace FakeRdb
 {
@@ -35,6 +36,72 @@ namespace FakeRdb
                 });
         }
 
+        [Fact]
+        public void FactMethodName()
+        {
+            using var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+            SetUp();
 
+            var expectedAlbums = new List<Album>
+            {
+                new ("Track 1", "Artist 1", 2021),
+                new ("Track 2", "Artist 2", 2022),
+                new ("Track 3", "Artist 3", 2023)
+            };
+
+            // Act
+            var actualAlbums = ReadAlbums(connection);
+
+            // Assert
+            actualAlbums.Should().BeEquivalentTo(expectedAlbums);
+            return;
+
+            void InsertTracks(SqliteCommand insertRow, string title, string artist, int year)
+            {
+                insertRow.Parameters.AddWithValue("@Title", title);
+                insertRow.Parameters.AddWithValue("@Artist", artist);
+                insertRow.Parameters.AddWithValue("@Year", year);
+                insertRow.ExecuteNonQuery();
+                insertRow.Parameters.Clear();
+            }
+
+            static IEnumerable<Album> ReadAlbums(SqliteConnection conn)
+            {
+                using var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT Title, Artist, Year " +
+                                  "FROM Album";
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    yield return new Album(
+                        reader.GetString(0),
+                        reader.GetString(1), 
+                        reader.GetInt32(2));
+                }
+
+            }
+
+            void SetUp()
+            {
+                using var createTable = connection.CreateCommand();
+                createTable.CommandText =
+                    "CREATE TABLE Album (" +
+                    "Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Title TEXT, " +
+                    "Artist TEXT, " +
+                    "Year INTEGER)";
+                createTable.ExecuteNonQuery();
+                using var insertRow = connection.CreateCommand();
+                insertRow.CommandText =
+                    "INSERT INTO Album (Title, Artist, Year) " +
+                    "VALUES (@Title, @Artist, @Year)";
+
+                InsertTracks(insertRow, "Track 1", "Artist 1", 2021);
+                InsertTracks(insertRow, "Track 2", "Artist 2", 2022);
+                InsertTracks(insertRow, "Track 3", "Artist 3", 2023);
+            }
+        }
+        public record Album(string Title, string Artist, int Year);
     }
 }
