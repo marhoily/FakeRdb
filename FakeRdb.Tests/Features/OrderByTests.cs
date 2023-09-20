aea94e8b759f8108d0dc3b6a2b8f34da87eb2dd9
Ilya <marhoily@hotmail.com>
2023-09-20 22:14:06 +0300

    + Failing test: order by

---
+++ FakeRdb.Tests/Features/OrderByTests.cs
@@ -0,0 +1,20 @@
+using Xunit.Abstractions;
+
+namespace FakeRdb.Tests;
+
+public sealed class OrderByTests : ComparisonTestBase
+{
+    public OrderByTests(ITestOutputHelper output) : base(output)
+    {
+        Sqlite.SeedHeterogeneousData();
+        Sut.SeedHeterogeneousData();
+    }
+
+    [Theory]
+    [InlineData("Order by", "SELECT * FROM T ORDER BY X")]
+    public void F(string d, string sql)
+    {
+        CompareAgainstSqlite(sql, d);
+    }
+
+}
