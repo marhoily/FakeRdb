64bf04b58c413a7bd65292b0259ca8f926aed4d8
Ilya <marhoily@hotmail.com>
2023-09-20 10:11:00 +0300

    + Numeric functions

---
+++ FakeRdb.Tests/Libraries/NumericTests.cs
@@ -0,0 +1,95 @@
+﻿namespace FakeRdb.Tests.Libraries;
+
+public sealed class NumericTests
+{
+    [Theory]
+    [InlineData("100e-3", false)]
+    [InlineData("100e-1", true)]
+    [InlineData("1.1e3", true)]
+    [InlineData("1.1", false)]
+    [InlineData("1", true)]
+    [InlineData("1e3", true)]
+    // Negative
+    [InlineData("-1", true)]
+    [InlineData("-1.1", false)]
+    [InlineData("-1.1e3", true)]
+    // Zero
+    [InlineData("0", true)]
+    [InlineData("0.0", true)]
+    [InlineData("0e3", true)]
+    [InlineData("0.0e3", true)]
+    [InlineData("1E3", true)]
+    // Small fractions
+    [InlineData("1.000000000000001", false)]
+    [InlineData("1.0000000000000001", false)]  
+    // Very large number, close to the upper limit of double.
+    [InlineData("1e308", true)]  
+    // Very close to zero, lower limit of positive values for double.
+    [InlineData("1e-324", false)]  
+
+    [InlineData("1e", false)]  // Incomplete scientific notation.
+    [InlineData("e1", false)]  // Invalid format.
+    [InlineData("1..1", false)]  // Multiple decimal points.
+    [InlineData(".1e3", true)]  // Starts with a decimal point.
+    [InlineData(".", false)]  // Just a decimal point.
+    public void IsInteger(string text, bool isInteger)
+    {
+        text.IsInteger().Should().Be(isInteger);
+    }
+
+    [Theory]
+    [InlineData(1, true)]
+    [InlineData(1.1, false)]
+    [InlineData(-1, true)]
+    [InlineData(0, true)]
+    [InlineData(1.00001, false)]
+    [InlineData(1.99999, false)]
+    [InlineData(0.00001, false)]
+    public void DecimalIsInteger(decimal value, bool isInteger)
+    {
+        value.IsInteger().Should().Be(isInteger);
+    }
+    [Fact]
+    public void SmallDecimalIsInteger()
+    {
+        1.0000000000000000000001M.IsInteger().Should().Be(false);
+    }
+    [Fact]
+    public void BigDecimalIsInteger()
+    {
+        1.9999999999999999999999M.IsInteger().Should().Be(false);
+    }
+
+    [Theory]
+    [InlineData(1.0, true)]
+    [InlineData(1.1, false)]
+    [InlineData(-1.0, true)]
+    [InlineData(0.0, true)]
+    [InlineData(double.MaxValue, false)] // double.MaxValue has a fractional part
+    [InlineData(double.MinValue, false)] // double.MinValue has a fractional part
+    [InlineData(1.00001, false)]
+    [InlineData(1.99999, false)]
+    [InlineData(0.00001, false)]
+    [InlineData(1.0000000000001, false)] // Using values relevant to double's precision
+    [InlineData(1.9999999999999, false)]
+    public void DoubleIsInteger(double value, bool isInteger)
+    {
+        value.IsInteger().Should().Be(isInteger);
+    }
+    [Theory]
+    [InlineData(1.0, true)]
+    [InlineData(1.1, false)]
+    [InlineData(-1.0, true)]
+    [InlineData(0.0, true)]
+    [InlineData(double.MaxValue, false)] // double.MaxValue has a fractional part
+    [InlineData(double.MinValue, false)] // double.MinValue has a fractional part
+    [InlineData(1.00001, false)]
+    [InlineData(1.99999, false)]
+    [InlineData(0.00001, false)]
+    [InlineData(1.0000000000001, true)] // what would be good fo double overflows float
+    [InlineData(1.9999999999999, true)]
+    public void FloatIsInteger(float value, bool isInteger)
+    {
+        value.IsInteger().Should().Be(isInteger);
+    }
+}
--- FakeRdb/Processors/Utils/TypeExt.cs
+++ FakeRdb/Processors/Utils/TypeExt.cs
@@ -1,7 +1,98 @@
+using System.Globalization;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
 namespace FakeRdb;
 
-public static class TypeExt
+public static partial class TypeExt
 {
+    // Numeric strings consist of optional sign,
+    // any number of digits, optional decimal part and optional
+    // exponential part. Thus +0123.45e6 is a valid numeric value.
+    [GeneratedRegex(@"^(((?!0)|[-+]|(?=0+\.))(\d*\.)?\d+(e\d+)?)$")]
+    private static partial Regex IsNumericRegex();
+
+    static bool IsNumeric(this string value)
+    {
+        return IsNumericRegex().IsMatch(value);
+    }
+    public static SqliteStorageType GetStorageTypeType(
+        this object? obj, SqliteTypeAffinity columnAffinity)
+    {
+        var dataAffinity = obj switch
+        {
+            null => SqliteStorageType.Null,
+            string s => s.IsNumeric() ? s.IsInteger()
+                ? SqliteStorageType.Integer
+                : SqliteStorageType.Real
+                : SqliteStorageType.Text,
+            int or 
+                long or 
+                byte or 
+                char or 
+                byte => SqliteStorageType.Integer,
+            decimal m => m.IsInteger() 
+                ? SqliteStorageType.Integer 
+                : SqliteStorageType.Real,
+            double d => d.IsInteger() 
+                ? SqliteStorageType.Integer 
+                : SqliteStorageType.Real,
+            float f => f.IsInteger() 
+                ? SqliteStorageType.Integer 
+                : SqliteStorageType.Real,
+            _ => throw new ArgumentOutOfRangeException(obj.GetType().Name)
+        };
+        return (dataAffinity, columnAffinity) switch
+        {
+            /* A column with TEXT affinity stores all data using
+             * storage classes NULL, TEXT or BLOB. If numerical 
+             * data is inserted into a column with TEXT affinity 
+             * it is converted into text form before being stored.*/
+            (SqliteStorageType.Null, SqliteTypeAffinity.Text)
+                => SqliteStorageType.Null,
+            (SqliteStorageType.Blob, SqliteTypeAffinity.Text)
+                => SqliteStorageType.Blob,
+            (_, SqliteTypeAffinity.Text)
+                => SqliteStorageType.Text,
+
+            /* A column with NUMERIC affinity may contain values
+             * using all five storage classes. 
+             *
+             * A column that uses INTEGER affinity behaves the same
+             * as a column with NUMERIC affinity.
+             */
+            (var n, SqliteTypeAffinity.Numeric or SqliteTypeAffinity.Integer)
+                => n,
+
+            (SqliteStorageType.Integer, SqliteTypeAffinity.Real)
+                => SqliteStorageType.Real,
+            (var r, SqliteTypeAffinity.Real)
+                => r,
+            /*
+             * A column with affinity BLOB does not prefer one storage
+             * class over another and no attempt is made to coerce
+             * data from one storage class into another.
+             */
+            var (i, _) => i
+        };
+    }
+
+    public static bool IsInteger(this string text)
+    {
+        return BigInteger.TryParse(text, NumberStyles.Float, CultureInfo.InvariantCulture, out _);
+    }
+    public static bool IsInteger(this double value)
+    {
+        return Math.Abs((long)value - value) <= double.Epsilon;
+    }
+    public static bool IsInteger(this float value)
+    {
+        return Math.Abs((long)value - value) <= double.Epsilon;
+    }
+    public static bool IsInteger(this decimal value)
+    {
+        return value % 1 == 0;
+    }
     public static DynamicType GetRuntimeType(this object? obj)
     {
         if (obj == null) return DynamicType.Null;
