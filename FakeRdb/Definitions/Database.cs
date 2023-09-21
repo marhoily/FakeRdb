9ab378443a84f54dc129023775fbf2341b9f75f7
Ilya <marhoily@hotmail.com>
2023-09-21 19:22:55 +0300

    refactor

--- FakeRdb/Definitions/Database.cs
+++ FakeRdb/Definitions/Database.cs
@@ -7,31 +7,18 @@ public sealed class Database : Dictionary<string, Table>
 
 public sealed class Table : List<Row>
 {
+    public TableSchema Schema { get; }
     public int Autoincrement { get; set; }
 
-    public Table(TableSchema schema)
-    {
-        Schema = schema;
-    }
-
-    public TableSchema Schema { get; }
+    public Table(TableSchema schema) => Schema = schema;
 }
 
-public sealed record Row(Table Table, object?[] Data)
+public sealed record Row(object?[] Data)
 {
     public object? this[Field field] => Data[field.ColumnIndex];
 }
 
-public sealed class TableSchema
-{
-    public Field[] Columns { get; }
+public sealed record TableSchema(Field[] Columns);
 
-    public TableSchema(Field[] columns)
-    {
-        Columns = columns;
-    }
-
-}
 public sealed record Field(int ColumnIndex, string Name, 
-    TypeAffinity FieldType, 
-    bool IsAutoincrement = false);
+    TypeAffinity FieldType, bool IsAutoincrement = false);
--- FakeRdb/Processing/TableOperations.cs
+++ FakeRdb/Processing/TableOperations.cs
@@ -3,6 +3,6 @@ namespace FakeRdb;
 public static class TableOperations
 {
     public static long Autoincrement(this Table table) => ++table.Autoincrement;
-    public static void Add(this Table table, object?[] oneRow) => table.Add(new Row(table, oneRow));
+    public static void Add(this Table table, object?[] oneRow) => table.Add(new Row(oneRow));
 
 }
