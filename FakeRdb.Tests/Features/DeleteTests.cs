16ee51df11af4ed56124e966613347fdf0e597e6
Ilya <marhoily@hotmail.com>
2023-09-21 20:17:35 +0300

    + FAILING: DeleteTests

---
+++ FakeRdb.Tests/Features/DeleteTests.cs
@@ -0,0 +1,16 @@
+namespace FakeRdb.Tests;
+
+public sealed class DeleteTests : ComparisonTestBase
+{
+    public DeleteTests(ITestOutputHelper output) : base(output)
+    {
+        Sqlite.Seed3Albums();
+        Sut.Seed3Albums();
+    }
+
+    [Fact]
+    public void Delete_All()
+    {
+        CompareAgainstSqlite("DELETE FROM Album");
+    }
+}
