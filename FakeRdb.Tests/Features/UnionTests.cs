61c83cc555dff519c8dad7e87ebf6679ceb77712
Ilya <marhoily@hotmail.com>
2023-09-22 21:04:29 +0300

    + FAILING test

---
+++ FakeRdb.Tests/Features/UnionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace FakeRdb.Tests;
+
+public sealed class UnionTests : ComparisonTestBase
+{
+    public UnionTests(ITestOutputHelper output) : base(output)
+    {
+        Sqlite.Seed3Albums();
+        Sut.Seed3Albums();
+    }
+    [Fact]
+    public void Max()
+    {
+        CompareAgainstSqlite(
+            """
+            SELECT Title AS TitleOrArtist, Year
+            FROM Album
+            UNION ALL
+            SELECT Artist AS TitleOrArtist, Year 
+            FROM Album
+            """);
+    }
+}
