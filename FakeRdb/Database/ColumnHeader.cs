0af801d78087bdf9795d447434340b5d0606d358
Ilya <marhoily@hotmail.com>
2023-09-25 22:07:46 +0300

    + Column

--- FakeRdb/Database/ColumnHeader.cs
+++ FakeRdb/Database/ColumnHeader.cs
@@ -2,3 +2,13 @@ namespace FakeRdb;
 
 public sealed record ColumnHeader(int ColumnIndex, string Name, 
     TypeAffinity ColumnType, bool IsAutoincrement = false);
+
+public sealed class Column : List<object?>
+{
+    public Column(ColumnHeader header)
+    {
+        Header = header;
+    }
+
+    public ColumnHeader Header { get; }
+}
--- FakeRdb/Database/Table.cs
+++ FakeRdb/Database/Table.cs
@@ -3,9 +3,23 @@ namespace FakeRdb;
 public sealed class Table : List<Row>
 {
     public TableSchema Schema { get; }
+    public Column[] Data { get; }
+
     private int _autoincrement;
 
-    public Table(TableSchema schema) => Schema = schema;
+    public Table(TableSchema schema)
+    {
+        Schema = schema;
+        Data = schema.Columns
+            .Select(col => new Column(col))
+            .ToArray();
+    }
+
     public long Autoincrement() => ++_autoincrement;
-    public void Add(object?[] oneRow) => Add(new Row(oneRow));
+    public void Add(object?[] oneRow)
+    {
+        Add(new Row(oneRow));
+        for (var i = 0; i < oneRow.Length; i++) 
+            Data[i].Add(oneRow[i]);
+    }
 }
