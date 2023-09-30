3b000c38f4589fcf31dcd181ae3304c120790bd6
Ilya <marhoily@hotmail.com>
2023-09-30 15:22:45 +0300

    + positional parameters

--- FakeRdb/AdoProvider/DbCommandExtensions.cs
+++ FakeRdb/AdoProvider/DbCommandExtensions.cs
@@ -12,6 +12,24 @@ public static class DbCommandExtensions
         dbParameter.Value = value;
         cmd.Parameters.Add(dbParameter);
     }
+    public static void AddPositionalParameters(this DbCommand cmd,
+        DbProviderFactory factory, object?[]? positionalBindings)
+    {
+        if (positionalBindings != null)
+            foreach (var binding in positionalBindings)
+            {
+                cmd.AddPositionalParameter(factory, binding);
+            }
+    }
+    public static void AddPositionalParameter(this DbCommand cmd,
+        DbProviderFactory factory, object? value)
+    {
+        var dbParameter =
+            factory.CreateParameter() ??
+            throw new InvalidOperationException("WTF?");
+        dbParameter.Value = value;
+        cmd.Parameters.Add(dbParameter);
+    }
 
     public static (DbDataReader?, Exception?) SafeExecuteReader(this DbCommand cmd)
     {
--- FakeRdb/AssemblyAttributes.cs
+++ FakeRdb/AssemblyAttributes.cs
@@ -1,11 +1,11 @@
 ﻿using System.Diagnostics;
 using FakeRdb;
 
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.ColumnExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.UnaryExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.LiteralExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.BinaryExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.BindExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.AggregateExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.ScalarExp))]
-[assembly: DebuggerDisplay("{FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.InExp))]
+[assembly: DebuggerDisplay("Column: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.ColumnExp))]
+[assembly: DebuggerDisplay("Unary: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.UnaryExp))]
+[assembly: DebuggerDisplay("Literal: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.LiteralExp))]
+[assembly: DebuggerDisplay("Binary: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.BinaryExp))]
+[assembly: DebuggerDisplay("Bind: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.BindExp))]
+[assembly: DebuggerDisplay("Aggregate: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.AggregateExp))]
+[assembly: DebuggerDisplay("Scalar: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.ScalarExp))]
+[assembly: DebuggerDisplay("In: {FakeRdb.DebugPrint.Print(this),nq}", Target = typeof(IR.InExp))]
